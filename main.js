bb4fcd82758ad602dede31d889ab552c29ff5722
M1r3 <101670312+LyuOSX@users.noreply.github.com>
2022-07-26 22:05:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ client.on('message', async message => {
     if(message.author.bot) return;
   
     if(message.content === ">report") {
-        snekfetch.get('https://pastebin.com/raw/HgMJxtLv').then(e => {
+        snekfetch.get('https://pastebin.com/raw/yourpastebinlol').then(e => {
             let dfgiij = new discord.MessageEmbed()
             .setTimestamp()
             .setTitle(`M1r3 Cybersec`)
